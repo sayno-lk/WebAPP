40d6fd910eef850d14a6e4ee75065438d35d2fd9
李凯 <719828476@qq.com>
2017-12-21 10:50:47 +0800

    12

---
+++ WebApp/baoye/js/bgMusic.js
@@ -0,0 +1,21 @@
+﻿var bgMusic = $("#bg-music")[0];
+bgMusic.autoplay = !0;
+bgMusic.loop = !0;
+function playMusic() {
+    bgMusic.play();
+}
+function pauseMusic() {
+    bgMusic.pause();
+}
+$(window).load(function () {
+    //playMusic();
+    //背景声音开关
+    $(".bgsoundsw").on('touchstart', function (e) {
+        if ($(this).children('dd').is(':hidden')) {
+            pauseMusic();
+        } else {
+            playMusic();
+        }
+        $(this).children().toggle();
+    });
+});
